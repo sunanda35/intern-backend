6c011e3e766a94ec4a8f8d9250606bd7d6e521e0
sunanda35 <sunandasamanta38@gmail.com>
2021-06-05 08:52:45 +0530

    update

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 const port = process.env.PORT || 3000
-
+app.get('/', (req, res) => {
+    res.send('Nothing is here bitch!')
+})
 app.get('/getall', async(req, res) => {
     await upload.find({}, (err, result) => {
         if (err) {
